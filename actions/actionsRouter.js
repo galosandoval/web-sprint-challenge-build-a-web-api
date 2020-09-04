7be682299ba50e5b1489b4f8977eecb163ba85b0
Galo Sandoval <galosan92@gmail.com>
2020-09-04 10:43:17 -0700

    finished middleware, get and post on actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -11,12 +11,28 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   actionsDB.get(id).then((user) => {
     res.status(200).json(user);
   });
 });
 
+router.post('/', validateAction, (req, res, next) => {
+  actionsDB.insert(req.body)
+  .then(action => {
+    res.status(201).json(action)
+  })
+})
 
+// Custom Middleware
+
+function validateAction(req, res, next) {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    res.status(404).json({ error: "id, description and notes required" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
